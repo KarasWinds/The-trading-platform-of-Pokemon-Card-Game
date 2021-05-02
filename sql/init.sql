bd35dc915f47eaffb1ac0662d05a7481363ad653
Minchi Chiu <minchilcs@gmail.com>
2021-05-02 21:02:33 +0800

    edit init sql script

--- sql/init.sql
+++ sql/init.sql
@@ -1,22 +1,13 @@
 CREATE DATABASE IF NOT EXISTS `pokemon`;
 USE `pokemon`;
 
-CREATE TABLE IF NOT EXISTS `traders` (
-  `trader_ID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-  `password` varchar(30) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-  PRIMARY KEY (`trader_ID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `order_buy` (
   `buy_ID` int(50) unsigned NOT NULL AUTO_INCREMENT,
   `trader_ID` smallint(5) unsigned NOT NULL,
   `card_ID` tinyint(3) unsigned NOT NULL,
   `price` tinyint(3) unsigned NOT NULL,
   `time` datetime NOT NULL,
-  PRIMARY KEY (`buy_ID`) USING BTREE,
-  KEY `FK_order_buy_traders` (`trader_ID`) USING BTREE,
-  CONSTRAINT `FK_order_buy_traders` FOREIGN KEY (`trader_ID`) REFERENCES `traders` (`trader_ID`)
+  PRIMARY KEY (`buy_ID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `order_sell` (
@@ -25,9 +16,7 @@ CREATE TABLE IF NOT EXISTS `order_sell` (
   `card_ID` tinyint(3) unsigned NOT NULL,
   `price` tinyint(3) unsigned NOT NULL,
   `time` datetime NOT NULL,
-  PRIMARY KEY (`sell_ID`) USING BTREE,
-  KEY `FK_order_sell_traders` (`trader_ID`) USING BTREE,
-  CONSTRAINT `FK_order_sell_traders` FOREIGN KEY (`trader_ID`) REFERENCES `traders` (`trader_ID`)
+  PRIMARY KEY (`sell_ID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `trades` (
